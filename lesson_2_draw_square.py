a7956382788b1c8f22599b7b5acdaaf8c7c9919e
7ege <egemansuroglu2008@gmail.com>
2025-08-06 22:50:21 +0100

    i made a square

---
+++ lesson_2_draw_square.py
@@ -0,0 +1,13 @@
+#Ders3
+#Kare cizimi yaptım.
+import turtle
+turtle.shape("turtle")
+turtle.shape("circle")
+turtle.shape("triangle")
+turtle.forward(200)
+turtle.left(90)
+turtle.fd(200)
+turtle.left(90)
+turtle.fd(200)
+turtle.left(90)
+turtle.fd(200)
