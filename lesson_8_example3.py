e53fcea4f2392ef973ea95f629f84da9f4e7e81e
7ege <egemansuroglu2008@gmail.com>
2025-08-06 23:28:02 +0100

    Which one is the biggest(max) number.

---
+++ lesson_8_example3.py
@@ -0,0 +1,9 @@
+n1=int(input("Please enter a number"))
+n2=int(input("Please enter a number"))
+n3=int(input("Please enter a number"))
+
+biggest=max(n1,n2,n3)  #max(n1, n2, n3) fonksiyonu, girilen 3 sayıdan en büyüğünü seçer.
+
+
+
+print("The biggest number is",biggest)
