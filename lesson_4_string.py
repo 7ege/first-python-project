757310eb3640dc7f21abb5ada3330b912c78ca2a
7ege <egemansuroglu2008@gmail.com>
2025-08-06 23:04:44 +0100

    Update lesson_4_string.py

--- lesson_4_string.py
+++ lesson_4_string.py
@@ -1,13 +1,13 @@
-#String Veri tipi 
-print("Ege Mansuroglu") #string dedigimiz sey "harflerin,rakamların,boslukların" bir araya gelmesiyle olusur. 
+#String 
+print("Ege Mansuroglu")      #(turkısh)string dedigimiz sey "harflerin,rakamların,boslukların" bir araya gelmesiyle olusur. 
 Ege Mansuroglu 
-type("Ege Mansuroglu")  #type = tipi ne
+type("Ege Mansuroglu")  
 <class 'str'>
 ad="Ege Mansuroglu"
 ad
 'Ege Mansuroglu'
-ad[0] #0'dan baslar.       sanki 0==1 gibi dusunulur ve bosluklarda sayılır.
-'E'   #0'da E'dir
+ad[0]                        #0'dan baslar.       sanki 0==1 gibi dusunulur ve bosluklarda sayılır.
+'E'                          #0'da E'dir
 ad[1]
 'g'
 ad[2]
@@ -15,3 +15,4 @@ ad[2]
 ad[13]
 'u'
 ad[14]
+
