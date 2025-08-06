a39aba226cf8fcc5cac1cd955e2e6513e7ca0a11
7ege <egemansuroglu2008@gmail.com>
2025-08-06 23:12:13 +0100

    Average age (example)

---
+++ lesson_8_example.py
@@ -0,0 +1,11 @@
+#input() her zaman metin (string) döner.
+#Sayıya çevirmek istersen → int() veya float() kullanırsın.
+#Metin olarak bırakacaksan → sadece input(...) yeter.
+
+age1=int(input("Enter Number"))
+age2=int(input("Enter number"))
+age3=int(input("Enter number"))
+
+avr=(age1+age2+age3)/3
+
+print("Average:",avr)
