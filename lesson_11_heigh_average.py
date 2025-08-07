d9470d5a0646140640cda2a684db36bca94f573d
ege <egemansuroglu2008@gmail.com>
2025-08-07 22:26:32 +0100

    Update lesson_11_heigh_average.py

--- lesson_11_heigh_average.py
+++ lesson_11_heigh_average.py
@@ -1,12 +1,13 @@
 #Boy ortalaması ORNEK
-alex=int(input("Ahmet boyun kaç?"))
-hagi=int(input("Ayse boyun kaç?"))
-henry=int(input("Fatma boyun kaç?"))
-millie=int(input("Hayriye boyun kaç?"))
+alex=int(input("What is your height"))
+hagi=int(input("What is your height?"))
+henry=int(input("What is your height?"))
+millie=int(input("What is your height?"))
 
 
 avr=(alex+hagi+henry+millie)/4
 
 print("Average of the 4 friends is",avr)
+
 
 
