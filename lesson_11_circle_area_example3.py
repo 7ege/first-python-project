a13cdcb0c3a081c633a86777eacfead22a7b4c4d
ege <egemansuroglu2008@gmail.com>
2025-08-07 22:43:27 +0100

    Circle and Area Calculation

---
+++ lesson_11_circle_area_example3.py
@@ -0,0 +1,5 @@
+# Circle and Area Calculation
+radius = int(input("Please enter the radius: "))
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius ** 2
+print("Circumference:", circumference, "and Area:", area)
