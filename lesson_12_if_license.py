d2f9f5791cf3b693e4898ad117c255e40d30ea2e
ege <egemansuroglu2008@gmail.com>
2025-08-08 22:18:42 +0100

    "If"

---
+++ lesson_12_if_license.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+
+if age>=18:
+    print("You can get your driver’s license: )")
+    print("Do you have a car?")
+else:
+    print("You are not eligible to get a driver's license:")
+
