33422c58ebbdea9cc99c6082db618c444cb8e3bd
ege <egemansuroglu2008@gmail.com>
2025-08-08 22:21:12 +0100

    Learning "or" with food example

---
+++ lesson_15_or_food_ex.py
@@ -0,0 +1,8 @@
+#OR
+burger=input("Do you wanna eat burger?")
+pasta=input("Do you wanna eat pasta?")
+
+if (burger=="yes") or (pasta=="yes"):
+    print("You can order your meal:)")
+else:
+    print("Sorry we dont have any other foods...")
