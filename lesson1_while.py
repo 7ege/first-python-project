939ff0d79728d0ddf0cc7a0da1cd6f14c53e20a0
ege <egemansuroglu2008@gmail.com>
2025-08-11 21:05:47 +0100

    I'm learning WHILE

---
+++ lesson1_while.py
@@ -0,0 +1,10 @@
+#şartın baslangıc degeri
+sayac=1
+
+#sayac 5ten küçük ya da eşit oldugu surece
+while sayac<=7:
+    print("Merhaba Ege",sayac)
+    sayac=sayac+1  #sayacın degerini 1 arttırır
+
+#döngü bittiginde gelecek
+print("Döngü bitti")  
