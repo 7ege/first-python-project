8ecad8401441d66dbf722ad241243f0a475385e2
ege <egemansuroglu2008@gmail.com>
2025-08-26 22:38:27 +0100

    I made a triangle.

---
+++ lesson_4_for_example2.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.color("red")
+turtle.bgcolor("yellow")
+for x in range(3):
+    turtle.write("x:"+str(x))
+    turtle.fd(100)
+    turtle.left(120)
+    
+
+turtle.color("red")
+turtle.bgcolor("yellow")
+for x in range(3):
+    turtle.write("x:"+str(x))
+    turtle.fd(200)
+    turtle.left(120)
+    
