e08617c37bcab01928432ae56e3632ba29386100
ege <egemansuroglu2008@gmail.com>
2025-09-22 17:05:22 +0100

    First lesson

---
+++ lesson1_numbers.py
@@ -0,0 +1,52 @@
+ad="Gökhan SU"
+
+#first character
+print("İlk karakter",ad[0])
+
+#second character
+print("İkinci karakter",ad[1])
+
+#son karakter
+print("son karakter",ad[8])
+
+#ilk üç karakter 1. metot
+print("İlk üç karakter",ad[0:3])
+
+
+#ilk üç karakter 2. metot
+print("İlk üç karakter",ad[:3])
+
+#son iki karakter 1. metot
+print("Son iki karakter",ad[7:9])
+
+#son iki karakter 2. metot
+print("Son iki karakter",ad[7:])
+
+#tersten gelme
+print("Son karakter",ad[-1])
+
+#tersten gelme
+print("Sondan 2. karakter",ad[-2])
+
+
+
+
+
+ad="Ege Mansuroglu"
+
+print("Ilk karakter",[0])
+
+print("Ikinci karakter",[1])
+
+print("Ilk uc karakter",[:3])
+
+print("Son iki karakter",[11:13])
+
+
+
+
+
+
+
+
+
