e9420fa68e87a377c798c293db78ca18d5953ba7
ege <egemansuroglu2008@gmail.com>
2025-09-22 17:08:27 +0100

    Example with characters

---
+++ section4/lesson1_example.py
@@ -0,0 +1,11 @@
+soyisim="James"
+harf=soyisim[3:5]
+
+gir=input("Soy isiminin son 2. ve 3. harflerini giriniz")
+
+if gir==harf:
+    print("Tebrikler dogru girdiniz")
+
+else:
+    print("Hatali girdiniz ")
+
