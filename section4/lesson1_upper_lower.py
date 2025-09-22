c0fc22a1b81bab203a3350b9158eb524970b5f09
ege <egemansuroglu2008@gmail.com>
2025-09-22 17:08:58 +0100

    Learning upper. and lower.

---
+++ section4/lesson1_upper_lower.py
@@ -0,0 +1,25 @@
+takim="galatasaray"
+ad="EGE"
+
+#takimin buyuk hali
+print(takim.upper())     #GALATASARAY
+
+#adin kucuk hali
+print(ad.lower())        #ege
+
+#takim buyuk harflerden mi olusur?
+print(takim.isupper())  #False
+
+#takim kucuk harflerden mi olusur?
+print(takim.islower())  #True
+
+#ad buyuk harflerden mi olusur?
+print(ad.isupper())  #True
+
+#takim kucuk harflerden mi olusur?
+print(ad.islower())  #False
+
+
+
+
+
