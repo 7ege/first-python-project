9689f478e3b606a53edccd1d718991545a17bbbe
ege <egemansuroglu2008@gmail.com>
2025-10-04 22:03:36 +0100

    Learning split

---
+++ section4/lesson3_split.py
@@ -0,0 +1,12 @@
+#birlestirme
+cumle="ege"+" " +str(17)+ " yasinda"
+print(cumle)
+
+
+#parcalama(split)
+dogumtarihi="10/03/2008"
+parcala=dogumtarihi.split("/")
+
+print("Gun",parcala[0])
+print("Ay",parcala[1])
+print("Yil",parcala[2])
