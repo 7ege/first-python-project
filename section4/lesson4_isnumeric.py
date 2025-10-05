2b4e852ff007c141340ffb3a764ce75c7a24982f
ege <egemansuroglu2008@gmail.com>
2025-10-05 21:52:26 +0100

    Learning isnumeric

---
+++ section4/lesson4_isnumeric.py
@@ -0,0 +1,12 @@
+#isnumeric()
+#0,1,2,3,4,5,6,7,8,9 dan mi ibarettir?
+
+print("15".isnumeric())       #TRUE
+
+print("15a".isnumeric())      #FALSE
+
+print("15?".isnumeric())      #FALSE
+
+print("15 ".isnumeric())      #FALSE
+
+
