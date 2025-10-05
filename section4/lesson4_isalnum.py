88e5c761b388905e0853ae4a2df736071dc25268
ege <egemansuroglu2008@gmail.com>
2025-10-05 21:54:08 +0100

    Learning isalnum

---
+++ section4/lesson4_isalnum.py
@@ -0,0 +1,10 @@
+#isalnum()   HARF ya da SAYIDAN olusuyor mu?
+
+
+print("tky20".isalnum())    #TRUE
+
+print("thy".isalnum())      #TRUE
+
+print("0535".isalnum())     #TRUE
+
+print("tky-25".isalnum())   #FALSE
