89d384e1aa845a7e419c8e33d50654001571df32
ege <egemansuroglu2008@gmail.com>
2025-10-05 21:53:32 +0100

    Learning isalpha

---
+++ section4/lesson4_isalpha.py
@@ -0,0 +1,13 @@
+#isalpha()
+#isnumeric()
+#isalnum()
+
+#isalpha
+#sadece HARFLERDEN olusuyor
+print("ege".isalpha())         #TRUE
+
+print("ege mansur".isalpha())  #FALSE
+
+print("ege2".isalpha())        #FALSE
+
+print("ege-".isalpha())        #FALSE
